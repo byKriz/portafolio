93c076742d87990693c922158562683b68e8dd51
byKriz <cristianesquivelh@hotmail.com>
2022-10-13 19:04:42 -0400

    creando el title de los proyectos

---
+++ src/components/MyWorks/MyWorks.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./MyWorks.module.scss";
+
+export const MyWorks = () => {
+  return (
+    <section className={styles.MyWorks} id="projects">
+      <div className={styles.myWorks__title}>
+        <h2>Projects I have created</h2>
+      </div>
+      <div className={styles.myWorks__container}>
+
+      </div>
+    </section>
+  );
+};
--- src/container/Home/Home.jsx
+++ src/container/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { HomePage } from '../../components/HomePage/HomePage'
+import { MyWorks } from '../../components/MyWorks/MyWorks'
 import styles from "./Home.module.scss"
 
 export const Home = () => {
   return (
     <main className={styles.content}>
         <HomePage />
+        <MyWorks />
     </main>
   )
 }
