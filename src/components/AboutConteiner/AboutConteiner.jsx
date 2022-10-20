563ce38f65e8b41a1e2fdcda57c6a2d01c88a9dd
byKriz <cristianesquivelh@hotmail.com>
2022-10-20 12:01:35 -0400

    añadiendo cards

--- src/components/AboutConteiner/AboutConteiner.jsx
+++ src/components/AboutConteiner/AboutConteiner.jsx
@@ -1,14 +1,12 @@
 import React from "react";
+import { TechnologyCard } from "../TechnologyCard/TechnologyCard";
 import styles from "./AboutConteiner.module.scss";
 
 export const AboutConteiner = () => {
   return (
     <section className={styles.AboutConteiner} id="about">
       <div className={styles.about_title}>
-        <h2>
-          Let me tell you a little
-          about myself
-        </h2>
+        <h2>Let me tell you a little about myself</h2>
       </div>
       <div className={styles.about_text}>
         <h4>Welcome,</h4>
@@ -24,6 +22,22 @@ export const AboutConteiner = () => {
       {/* <figure>
             <img src="" alt="" />
         </figure> */}
+      <div className={styles.cardContainer}>
+        <TechnologyCard
+          text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo
+          beatae corporis cupiditate recusandae"
+        />
+        <TechnologyCard
+          tech="Tailwind"
+          text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo
+          beatae corporis cupiditate recusandae"
+        />
+        <TechnologyCard
+          tech="Sass"
+          text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo
+          beatae corporis cupiditate recusandae"
+        />
+      </div>
     </section>
   );
 };
---
+++ src/components/TechnologyCard/TechnologyCard.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styles from "./TechnologyCard.module.scss";
+import { FaReact, FaSass } from "react-icons/fa";
+import { SiTailwindcss } from "react-icons/si";
+
+export const TechnologyCard = ({ tech = "React", text }) => {
+  const technologies = {
+    React: {
+      icon: <FaReact />,
+      style: styles.react,
+    },
+    Tailwind: {
+      icon: <SiTailwindcss />,
+      style: styles.tailwind,
+    },
+    Sass: {
+      icon: <FaSass />,
+      style: styles.sass,
+    },
+  };
+
+  return (
+    <div className={styles.TechnologyCard}>
+      <div className={`${styles.cardHeader} ${technologies[tech].style}`}>
+        {technologies[tech].icon}
+      </div>
+      <div className={styles.cardContent}>
+        <h1 className={styles.cardTitle}>{tech}</h1>
+        <p className={styles.cardText}>{text}</p>
+        {/* <button className="card-btn one">code</button> */}
+      </div>
+    </div>
+  );
+};
