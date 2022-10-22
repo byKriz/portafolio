9aa267a1beb6d2e8a427e86201c744f0dad18cf8
byKriz <cristianesquivelh@hotmail.com>
2022-10-22 18:24:40 -0400

    agregando mq para la versión de escritorio

--- src/components/AboutConteiner/AboutConteiner.jsx
+++ src/components/AboutConteiner/AboutConteiner.jsx
@@ -8,35 +8,37 @@ export const AboutConteiner = () => {
       <div className={styles.about_title}>
         <h2>Let me tell you a little about myself</h2>
       </div>
-      <div className={styles.about_text}>
-        <h4>Welcome,</h4>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo
-          beatae corporis cupiditate recusandae. Cum voluptates est facere iure,
-          sapiente exercitationem repudiandae, enim minus sequi eum quasi
-          incidunt dolores, necessitatibus quisquam! Commodi quasi nisi
-          corrupti. Officiis voluptatem at eum cum minima eligendi, atque,
-          quibusdam
-        </p>
-      </div>
-      {/* <figure>
+      <div className={styles.about_containt}>
+        <div className={styles.about_text}>
+          <h4>Welcome,</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo
+            beatae corporis cupiditate recusandae. Cum voluptates est facere
+            iure, sapiente exercitationem repudiandae, enim minus sequi eum
+            quasi incidunt dolores, necessitatibus quisquam! Commodi quasi nisi
+            corrupti. Officiis voluptatem at eum cum minima eligendi, atque,
+            quibusdam
+          </p>
+        </div>
+        {/* <figure>
             <img src="" alt="" />
         </figure> */}
-      <div className={styles.cardContainer}>
-        <TechnologyCard
-          text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo
+        <div className={styles.cardContainer}>
+          <TechnologyCard
+            text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo
           beatae corporis cupiditate recusandae"
-        />
-        <TechnologyCard
-          tech="Tailwind"
-          text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo
+          />
+          <TechnologyCard
+            tech="Tailwind"
+            text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo
           beatae corporis cupiditate recusandae"
-        />
-        <TechnologyCard
-          tech="Sass"
-          text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo
+          />
+          <TechnologyCard
+            tech="Sass"
+            text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo
           beatae corporis cupiditate recusandae"
-        />
+          />
+        </div>
       </div>
     </section>
   );
