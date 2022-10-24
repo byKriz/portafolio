f6dcc8b0b0ce9e18d984fb9a653177252cbdf0f3
byKriz <cristianesquivelh@hotmail.com>
2022-10-24 06:31:39 -0400

    ajustando el about y agregando imagen de perfil

--- src/components/AboutConteiner/AboutConteiner.jsx
+++ src/components/AboutConteiner/AboutConteiner.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { TechnologyCard } from "../TechnologyCard/TechnologyCard";
 import styles from "./AboutConteiner.module.scss";
+import profile from "@assets/profile/profile.png";
 
 export const AboutConteiner = () => {
   return (
@@ -9,16 +10,34 @@ export const AboutConteiner = () => {
         <h2>Let me tell you a little about myself</h2>
       </div>
       <div className={styles.about_containt}>
-        <div className={styles.about_text}>
-          <h4>Welcome,</h4>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo
-            beatae corporis cupiditate recusandae. Cum voluptates est facere
-            iure, sapiente exercitationem repudiandae, enim minus sequi eum
-            quasi incidunt dolores, necessitatibus quisquam! Commodi quasi nisi
-            corrupti. Officiis voluptatem at eum cum minima eligendi, atque,
-            quibusdam
-          </p>
+        <div className={styles.profile_content}>
+          <div className={styles.img_container}>
+            <img src={profile} alt="" className={styles.profile_img} />
+          </div>
+          <div className={styles.about_text}>
+            <h4>Welcome,</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Molestiae, nobis modi voluptas tempore laudantium debitis quia
+              consectetur itaque perspiciatis ratione aliquid possimus
+              temporibus labore mollitia recusandae sunt doloremque id
+              laboriosam. Quia, iusto. Tempore nisi dolores nam veniam
+              cupiditate repudiandae pariatur dolorem earum totam ipsum!
+              Corrupti reiciendis soluta, nesciunt facere suscipit magni,
+              quibusdam recusandae asperiores eos modi eveniet perferendis
+              obcaecati vel? Et, nulla! Nostrum itaque incidunt porro? Commodi,
+              dolorem quae sequi deserunt et earum, quis tempore illo aperiam
+              iusto explicabo nulla magnam. Ad qui error consequuntur ut magni
+              officiis impedit quas. Expedita doloribus itaque provident ullam
+              non repellendus, quisquam esse iste officia. Dolor sit consequatur
+              sed! Dolor mollitia reiciendis, accusantium quam tenetur
+              accusamus. Repellat iure praesentium ab libero reiciendis error
+              odit! Magnam reprehenderit asperiores incidunt in! Recusandae
+              inventore officia natus nostrum odit eligendi adipisci esse
+              quisquam fuga commodi. Esse ipsum quis, aliquam provident culpa ad
+              iure officiis facere tempore, tenetur fuga.
+            </p>
+          </div>
         </div>
         {/* <figure>
             <img src="" alt="" />
